50bba623c6f0157ed278dfb81fdc5f0bbeadd406
Aline <alineraldif@gmail.com>
2025-04-21 18:13:08 -0300

    feat: Implementação da função showSearch()

--- projeto1/assets/js/main.js
+++
@@ -1,8 +0,0 @@
-document.getElementById("toggleMenuBtn").addEventListener("click", function() {
-    var menu = document.getElementById("categoriesMenu");
-    if (menu.style.display === "none" || menu.style.display === "") {
-        menu.style.display = "flex";
-    } else {
-        menu.style.display = "none";
-    }
-});
